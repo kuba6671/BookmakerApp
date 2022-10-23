0a9c0c96f8907fccd7395959cb815a21dafb601e
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2022-10-23 15:29:50 +0200

    feature/Added event controller

---
+++ src/main/java/com/bookmakerApp/controller/EventController.java
@@ -0,0 +1,28 @@
+package com.bookmakerApp.controller;
+
+import com.bookmakerApp.facade.dtos.FootballEventModelDto;
+import com.bookmakerApp.facade.impl.FootballEventFacadeImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin
+public class EventController {
+
+    private final FootballEventFacadeImpl footballEventFacade;
+
+    @GetMapping("/events/unfinishedFootballEvents")
+    public List<FootballEventModelDto> getUnfinishedFootballEvents(){
+        return footballEventFacade.getUnfinishedFootballEvents();
+    }
+
+    @GetMapping("/events/finishedFootballEvents")
+    public List<FootballEventModelDto> getFinishedFootballEvents(){
+        return footballEventFacade.getFinishedFootballEvents();
+    }
+}
