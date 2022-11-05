5e8f0d3c9285046e96749c297c3da7d627cb47d9
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2022-11-05 16:09:04 +0100

    bug/fixed sportModel relationship

--- src/main/java/com/bookmakerApp/model/SportModel.java
+++ src/main/java/com/bookmakerApp/model/SportModel.java
@@ -3,13 +3,17 @@ package com.bookmakerApp.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class SportModel {
     @Id
     private Long idSport;
+    private String sportName;
+    @ManyToOne
+    @JoinColumn(name = "idEvent")
+    private EventModel event;
 }
