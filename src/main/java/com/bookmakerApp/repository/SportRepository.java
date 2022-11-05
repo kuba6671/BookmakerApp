96cf841e10db573397ee40ca6e8cf90e101472c8
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2022-11-05 16:19:54 +0100

    feature/added SportRepository and SportService

--- src/main/java/com/bookmakerApp/repository/SportRepository.java
+++ src/main/java/com/bookmakerApp/repository/SportRepository.java
@@ -3,5 +3,8 @@ package com.bookmakerApp.repository;
 import com.bookmakerApp.model.SportModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SportRepository extends JpaRepository<SportModel, Long> {
+    public List<SportModel> getSportModelsBySportName(String sportName);
 }
---
+++ src/main/java/com/bookmakerApp/service/interfaces/SportService.java
@@ -0,0 +1,10 @@
+package com.bookmakerApp.service.interfaces;
+
+import com.bookmakerApp.model.SportModel;
+
+import java.util.List;
+
+
+public interface SportService {
+    List<SportModel> getFootballMatches();
+}
