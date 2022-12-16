1927a70266635a9683be877887730749e404fb39
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2022-12-16 21:40:14 +0100

    added example of get a token from local storage

---
+++ src/main/java/com/bookmakerApp/view/BookmakerClientApp/BookmakerClientApp/Data/Extension/LocalStorageExtension.cs
@@ -0,0 +1,37 @@
+﻿using Hanssens.Net;
+using System;
+
+namespace BookmakerClientApp.Data.Extension
+{
+    public sealed class LocalStorageExtension
+    {
+        private static LocalStorage localStorage;
+
+        private static LocalStorageExtension localStorageExtension;
+
+        private LocalStorageExtension()
+        {
+            if(localStorage == null)
+                localStorage = new LocalStorage();
+        }
+
+        public static LocalStorageExtension GetInstance()
+        {
+            if(localStorageExtension == null)
+            {
+                localStorageExtension = new LocalStorageExtension();
+            }
+            return localStorageExtension;
+        }
+
+        public String Get(string key)
+        {
+            return localStorage.Get(key).ToString();
+        }
+
+        public void Store(string key, string value)
+        {
+            localStorage.Store(key,value);
+        }
+    }
+}
---
+++ src/main/java/com/bookmakerApp/view/BookmakerClientApp/BookmakerClientApp/Data/Service/EventService.cs
@@ -0,0 +1,26 @@
+﻿using BookmakerClientApp.Data.Extension;
+using Hanssens.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BookmakerClientApp.Data.Service
+{
+    public class EventService
+    {
+        private readonly HttpClient httpClient;
+
+        public EventService()
+        {
+            this.httpClient = new HttpClient();
+        }
+
+        public String getToken()
+        {
+            LocalStorageExtension localStorage = LocalStorageExtension.GetInstance();
+            return localStorage.Get("Token");
+        }
+    }
+}
