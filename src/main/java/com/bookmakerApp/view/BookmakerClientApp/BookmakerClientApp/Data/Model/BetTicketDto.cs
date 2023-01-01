ef6ff79866170edc458428190a4f0c76ae819702
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2023-01-01 20:39:34 +0100

    updated BetTicket component

--- src/main/java/com/bookmakerApp/view/BookmakerClientApp/BookmakerClientApp/Data/Model/BetTicketDto.cs
+++ src/main/java/com/bookmakerApp/view/BookmakerClientApp/BookmakerClientApp/Data/Model/BetTicketDto.cs
@@ -11,14 +11,14 @@ namespace BookmakerClientApp.Data.Model
         private bool? success;
         private Decimal deposit;
         private Decimal toWin;
-        private Double odds;
+        private Double totalOdds;
         private DateTime date;
         List<FootballEventModel> events;
 
 
         public decimal Deposit { get => deposit; set => deposit = value; }
         public decimal ToWin { get => toWin; set => toWin = value; }
-        public double Odds { get => odds; set => odds = value; }
+        public double TotalOdds { get => totalOdds; set => totalOdds = value; }
         public DateTime Date { get => date; set => date = value; }
         public List<FootballEventModel> Events { get => events; set => events = value; }
         public bool? Finish { get => finish; set => finish = value; }
