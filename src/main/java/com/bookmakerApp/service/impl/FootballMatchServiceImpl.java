00825508b675c41f3dd7498dd40ff52c2ba7538f
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2023-01-28 20:24:36 +0100

    code refactoring

--- src/main/java/com/bookmakerApp/service/impl/FootballMatchServiceImpl.java
+++ src/main/java/com/bookmakerApp/service/impl/FootballMatchServiceImpl.java
@@ -4,22 +4,25 @@ import com.bookmakerApp.model.football.FootballMatchModel;
 import com.bookmakerApp.model.football.FootballTeamModel;
 import com.bookmakerApp.repository.FootballMatchRepository;
 import com.bookmakerApp.repository.FootballTeamRepository;
+import com.bookmakerApp.service.interfaces.FootballMatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class FootballMatchServiceImpl {
+public class FootballMatchServiceImpl implements FootballMatchService {
 
     private final FootballMatchRepository footballMatchRepository;
 
     private final FootballTeamRepository footballTeamRepository;
 
+    @Override
     public FootballMatchModel addFootballMatch(FootballMatchModel footballMatch) {
         footballMatch.setSportName("Piłka nożna");
         return footballMatchRepository.save(footballMatch);
     }
 
+    @Override
     public FootballTeamModel addFootballTeam(FootballTeamModel footballTeam) {
         return footballTeamRepository.save(footballTeam);
     }
