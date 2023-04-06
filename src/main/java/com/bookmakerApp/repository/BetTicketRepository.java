df62e6e7b30ab1829e69510cca325d3cdbc332a3
kuba6671 <kuba6671@gmail.com>
2023-04-06 13:40:05 +0200

    fix/fixed formatting

--- src/main/java/com/bookmakerApp/repository/BetTicketRepository.java
+++ src/main/java/com/bookmakerApp/repository/BetTicketRepository.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface BetTicketRepository extends JpaRepository<BetTicketModel, Long> {
     List<BetTicketModel> getBetTicketModelsByUserIdUser(Long id);
+
     List<BetTicketModel> getBetTicketModelsByUserIdUserAndSuccess(Long id, Boolean success);
+
     List<BetTicketModel> getBetTicketModelsByUserIdUserAndFinish(Long id, boolean finish);
+
     List<BetTicketModel> getBetTicketModelsByFinish(boolean finish);
+
     List<BetTicketModel> getBetTicketModelsByFinishAndAndResultIsChecked(boolean finish, boolean resultIsChecked);
 }
--- src/main/java/com/bookmakerApp/repository/FootballTeamRepository.java
+++ src/main/java/com/bookmakerApp/repository/FootballTeamRepository.java
@@ -3,5 +3,5 @@ package com.bookmakerApp.repository;
 import com.bookmakerApp.model.football.FootballTeamModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FootballTeamRepository extends JpaRepository <FootballTeamModel, Long> {
+public interface FootballTeamRepository extends JpaRepository<FootballTeamModel, Long> {
 }
