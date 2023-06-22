c1a532f4f4a39494c69124a1d1a51a7aceb62349
kuba6671 <33839399+kuba6671@users.noreply.github.com>
2023-06-22 17:45:37 +0200

    removed deprecated WebSecurityConfigurerAdapter

--- src/main/java/com/bookmakerApp/config/security/WebConfig.java
+++ src/main/java/com/bookmakerApp/config/security/WebConfig.java
@@ -1,8 +1,10 @@
 package com.bookmakerApp.config.security;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Component
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
